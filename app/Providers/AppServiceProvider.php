99572329f0d0c99a6ff8fe1d4d100b1db31caf62
efren0214 <efren@claritystack.com>
2023-05-22 17:55:50 +0800

    https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
 
@@ -20,6 +21,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        If(env('APP_ENV') !== 'local') { URL::forceSchema('https'); }
+        if (App::environment('production'))
+        {
+            URL::forceScheme('https');
+        }
     }
 }
